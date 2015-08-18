52a4d5365d80d3c9ac8447964c8a63df59db8d28
Tamara Broderick <tbroderick@csail.mit.edu>
2015-08-17 20:05:25 -0400

    Added package dependency for Dirichlet draws

--- ex3_largeK_distr.R
+++ ex3_largeK_distr.R
@@ -15,6 +15,9 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# useful for sampling from the Dirichlet distribution
+library(MCMCpack)
+
 # maximum number of data points to draw
 maxN = 1000
 
